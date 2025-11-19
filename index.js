4d6de46d7c7486793f762f3c212c3ad60a810c79
namanbachhawat <namanbachhawat@example.com>
2025-11-19 20:40:19 +0530

    Fix NativeWind v4 setup: added metro.config.js, removed Babel plugin, updated index.js, installed dependencies

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 import { registerRootComponent } from 'expo';
-import { NativeWindStyleSheet } from 'nativewind';
 import App from './App';
-
-NativeWindStyleSheet.begin();
 
 registerRootComponent(App);
